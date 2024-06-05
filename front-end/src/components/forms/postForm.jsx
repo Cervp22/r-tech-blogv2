12e50a7be36228c8ebc605417eed64b1d7a46fb5
Paul Cervantes <paulcervantes2244@gmail.com>
2024-06-05 16:59:54 -0700

    post value update

--- front-end/src/components/forms/postForm.jsx
+++ front-end/src/components/forms/postForm.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-import "../styles/postform.css"
-
+import "../styles/postform.css";
 
 export default function postForm(props) {
-  
+  const navigate = useNavigate();
+
   const [userPost, setUserPost] = useState();
-
-
 
   const handleInput = (e) => {
     setUserPost(e.target.value);
@@ -19,25 +18,29 @@ export default function postForm(props) {
     axios.defaults.withCredentials = true;
     const username = props.username;
     const userId = props.id;
-    try{
-       const post =  await axios({
-        method: "post",
-        url: 'http://localhost:3001/api/posts',
-        data:{
+    try {
+      if (userPost.value === "") {
+        alert("Please write some text before you submit to post");
+      } else {
+        const post = await axios({
+          method: "post",
+          url: "http://localhost:3001/api/posts",
+          data: {
             username,
             userId,
-            userPost
-        },
-       })
-
-    }catch(err){
-        console.log(err)
+            userPost,
+          },
+        });
+        navigate("/");
+      }
+    } catch (err) {
+      console.log(err);
     }
   }
 
   return (
     <>
-      <div className="postdiv" >
+      <div className="postdiv">
         <h1 className="posttitle">Post Here..</h1>
         <br />
         <form onSubmit={handleFormSubmit}>
@@ -48,7 +51,9 @@ export default function postForm(props) {
             className="userPostformta"
             name="userPost"
           />
-          <button type="submit">Submit</button>
+          <button type="submit" onClick={handleFormSubmit}>
+            Submit
+          </button>
         </form>
       </div>
     </>
